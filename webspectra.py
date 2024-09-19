f79757156c60d327a8718a13d4daf3fef23b9533
James Bronder <36022278+jbronder@users.noreply.github.com>
2024-09-19 14:11:01 -0700

    feat: added a spectra-only data CLI option
    Added support for users to request spectra series data only for a site.
    Users can now output specific response spectrum series in either JSON or
    CSV formats for ASCE 7-XX design standards where made available by the web
    service.

--- webspectra.py
+++ webspectra.py
@@ -7,10 +7,15 @@
 #------------------------------------------------------------------------------
 
 import argparse
+import csv
+import json
 import pprint
-import urllib.error
+import sys
 import urllib.parse
 import urllib.request
+
+from types import SimpleNamespace
+from urllib.error import URLError
 
 # Web Spectra Specific Modules
 from wdstable import DESCRIPTION_LABELS, METADATA_LABELS, Extractor, Table, \
@@ -20,9 +25,62 @@ def _output_user_request(input_parameters: dict[str, any]):
     print("User Input:")
     pprint.pprint(input_parameters)
 
+def unzip_and_collect(
+        spectrum: list[list[float]]
+        ) -> tuple[list[float],list[float]]:
+    """Converts a list of spectrum pairs into a tuple of two lists denoted as
+    'points_tuple' with tuple[0] representing the period and tuple[1]
+    representing the ordinates.
+
+    Parameters
+    ----------
+    spectrum : list[list[float]]
+
+    Returns
+    -------
+    points_tuple : tuple[list[float], list[float]]
+    """
+    periods = []
+    ordinates = []
+    for points in spectrum[1]:
+        periods.append(points[0])
+        ordinates.append(points[1]) 
+    return (periods, ordinates)
+
+def spectrum_to_json(spectrum: list[list[float]] | SimpleNamespace) -> str:
+    """Converts spectrum series data into a JSON string.
+
+    Parameters
+    ----------
+    spectrum : list[list[float]] | SimpleNamespace
+
+    Returns
+    -------
+    json_str : str
+
+    """
+    result = {}
+    if isinstance(spectrum[1], SimpleNamespace):
+        result['periods'] = spectrum[1].periods
+        result['ordinates'] = spectrum[1].ordinates
+    else:
+        points = unzip_and_collect(spectrum)
+        result['periods'] = points[0]
+        result['ordinates'] = points[1]
+    return json.dumps(result)
+
+def spectrum_to_csv_to_stdout(spectrum: list[list[float]] | SimpleNamespace):
+    csv_stdout = csv.writer(sys.stdout)
+    if isinstance(spectrum[1], SimpleNamespace): 
+        for i in range(len(spectrum[1].periods)):
+            csv_stdout.writerow([spectrum[1].periods[i], spectrum[1].ordinates[i]])
+    else:
+        points = unzip_and_collect(spectrum)
+        for i in range(len(points[0])):
+            csv_stdout.writerow([points[0][i], points[1][i]])
+
 def make_request(args: dict[str, any]):
     """Consolidates user arguments to make a well-formed query.
-
     Parameters
     ----------
     args : dict[str, any]
@@ -42,27 +100,56 @@ def make_request(args: dict[str, any]):
         FULL_URL = ROOT_URL + REF_DOCUMENT + '.json?' + PARAMETERS_ENCODED
         with urllib.request.urlopen(FULL_URL) as response:
             response_string = response.read().decode('utf-8')
-            if args.output == 'json':
-                print(response_string)
-            else:
+
+            # outputs only spectrum data
+            if args.spectrum:
                 ext = Extractor(response_string)
-            
-                in_headings = ("Input", "Values")
-                user_req = ext.extract_input()
-                tbl_in = Table(in_headings, user_req)
-                tbl_in.render()
+                spectrum_label_mapping = {
+                        "two-design": "twoPeriodDesignSpectrum",
+                        "two-mcer": "twoPeriodMCErSpectrum",
+                        "vert-design": "verticalDesignSpectrum",
+                        "vert-mcer": "verticalMCErSpectrum",
+                        "multi-design": "multiPeriodDesignSpectrum",
+                        "multi-mcer": "multiPeriodMCErSpectrum",
+                        "risk-targeted": "riskTargetedSpectrum",
+                        "84th": "eightyFourthSpectrum",
+                        }
+                all_spectra: list[tuple[any, ...]] = ext.extract_spectra()
 
-                out_headings = ("Parameters", "Values", "Description")
-                svs = ext.extract_svs()
-                out_svs = append_output_descriptions(svs, DESCRIPTION_LABELS)
-                tbl_out = Table(out_headings, out_svs)
-                tbl_out.render()
+                if len(all_spectra) == 0:
+                    print('No spectra series data is provided for this site.')
+                else:
+                    for spectrum in all_spectra:
+                        if spectrum[0] == spectrum_label_mapping[args.spectrum]:
+                            if args.output == 'json':
+                                print(spectrum_to_json(spectrum))
+                            else:
+                                spectrum_to_csv_to_stdout(spectrum) 
 
-                meta_headings = ("Metadata", "Values", "Description")
-                metadata_svs = ext.extract_metadata_svs()
-                out_metadata_svs = append_output_descriptions(metadata_svs, METADATA_LABELS)
-                tbl_meta = Table(meta_headings, out_metadata_svs)
-                tbl_meta.render()
+            # outputs everything but spectrum data
+            else:
+                if args.output == 'json':
+                    print(response_string)
+                else:
+                    ext = Extractor(response_string)
+                
+                    in_headings = ("Input", "Values")
+                    user_req = ext.extract_input()
+                    tbl_in = Table(in_headings, user_req)
+                    tbl_in.render()
+
+                    out_headings = ("Parameters", "Values", "Description")
+                    svs = ext.extract_svs()
+                    out_svs = append_output_descriptions(svs, DESCRIPTION_LABELS)
+                    tbl_out = Table(out_headings, out_svs)
+                    tbl_out.render()
+
+                    meta_headings = ("Metadata", "Values", "Description")
+                    metadata_svs = ext.extract_metadata_svs()
+                    out_metadata_svs = append_output_descriptions(
+                            metadata_svs, METADATA_LABELS)
+                    tbl_meta = Table(meta_headings, out_metadata_svs)
+                    tbl_meta.render()
     except URLError:
         raise
 
@@ -126,6 +213,15 @@ asce722_parser.add_argument(**latitude_arg_template)
 asce722_parser.add_argument(**longitude_arg_template)
 asce722_parser.add_argument(**risk_category_arg_template)
 asce722_parser.add_argument(**site_class_arg_template_three)
+asce722_parser.add_argument(
+    '-s', '--spectrum',
+    help='request spectrum only',
+    choices=['two-design', 'two-mcer',
+            'vert-design', 'vert-mcer', 
+            'multi-design', 'multi-mcer',
+            'risk-targeted', '84th',
+            ],
+    )
 asce722_parser.set_defaults(std='asce7-22', fn=make_request)
 
 # asce7-16 subcommand
@@ -138,6 +234,13 @@ asce716_parser.add_argument(**latitude_arg_template)
 asce716_parser.add_argument(**longitude_arg_template)
 asce716_parser.add_argument(**risk_category_arg_template)
 asce716_parser.add_argument(**site_class_arg_template_two)
+asce716_parser.add_argument(
+    '-s', '--spectrum',
+    help='request spectrum only',
+    choices=['two-design', 'two-mcer'
+             'vert-design', 'vert-mcer',
+             ],
+)
 asce716_parser.set_defaults(std='asce7-16', fn=make_request)
 
 # asce7-10 subcommand
@@ -150,6 +253,11 @@ asce710_parser.add_argument(**latitude_arg_template)
 asce710_parser.add_argument(**longitude_arg_template)
 asce710_parser.add_argument(**risk_category_arg_template)
 asce710_parser.add_argument(**site_class_arg_template_one)
+asce710_parser.add_argument(
+    '-s', '--spectrum',
+    help='request spectrum only',
+    choices=['two-design', 'two-mcer'],
+)
 asce710_parser.set_defaults(std='asce7-10', fn=make_request)
 
 # asce7-05 subcommand
@@ -162,6 +270,11 @@ asce705_parser.add_argument(**latitude_arg_template)
 asce705_parser.add_argument(**longitude_arg_template)
 asce705_parser.add_argument(**risk_category_arg_template)
 asce705_parser.add_argument(**site_class_arg_template_one)
+asce705_parser.add_argument(
+    '-s', '--spectrum',
+    help='request spectrum only',
+    choices=['two-design', 'two-mcer'],
+    )
 asce705_parser.set_defaults(std='asce7-05', fn=make_request)
 
 # nehrp-2020 subcommand
